3fc83b6281a158087cb5f6f63c672bc7f32b6039
Matthew Santiago <carag.matthew@gmail.com>
2020-10-25 01:43:44 -0500

    Aliasing the project :D

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import slothengine.Window;
-
-// TODO: Rename project to Oni-chan
-public class Main {
-    public static void main(String[] args) {
-        Window window = Window.get();
-        window.run();
-    }
-}
---
+++ src/main/java/OniChanApplication.java
@@ -0,0 +1,8 @@
+import slothengine.Window;
+
+public class OniChanApplication {
+  public static void main(String[] args) {
+    Window window = Window.get();
+    window.run();
+  }
+}
