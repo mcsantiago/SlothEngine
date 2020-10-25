a3092642e5e8153ba807cff056ae69aed97c13ef
Matthew Santiago <carag.matthew@gmail.com>
2020-10-25 01:40:37 -0500

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import slothengine.Window;
+
+// TODO: Rename project to Oni-chan
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
